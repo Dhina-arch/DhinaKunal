ee281fbcf768c0cece85e032a173b2ec8baf5902
Dhina-arch <ds8260@srmist.edu.in>
2025-09-11 09:06:04 +0530

    Delete batch2/ros2_ws directory

--- batch2/ros2_ws/build/robot_controller_ra3444/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/administrator/dhina/DhinaKunal/batch2/ros2_ws/install/robot_controller_ra3444'
